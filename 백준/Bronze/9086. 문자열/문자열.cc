4d2ab644f6c324ba294f27d0c76599a920e29020
김동우 <111382192+c1inamen@users.noreply.github.com>
2023-09-25 09:16:34 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void)
+{
+	int n;
+	string s;
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		cin >> s;
+		cout << s[0] << s[s.length()-1]<<endl;
+	}
+	return 0;
+}
