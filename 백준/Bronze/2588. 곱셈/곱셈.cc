a8dec490627e0b65a34e92998b800d6059ef2ccd
김동우 <111382192+c1inamen@users.noreply.github.com>
2023-09-25 10:04:01 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int A, B;
+	
+	cin >> A;
+	cin >> B;
+	cout << A * ((B % 100) % 10) << endl;
+	cout << A * ((B % 100) / 10) << endl;
+	cout << A * (B / 100) << endl;
+	cout << A * B;
+	return 0;
+}
