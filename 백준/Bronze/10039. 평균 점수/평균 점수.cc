dd28ad98474f13da6c8982137fde278bdf657a2d
spreadone <111382192+spread-one@users.noreply.github.com>
2023-10-22 15:48:12 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		int n;
+		cin >> n;
+		if (n < 40)
+			n = 40;
+		sum += n;
+	}
+	cout << sum / 5;
+	return 0;
+}
