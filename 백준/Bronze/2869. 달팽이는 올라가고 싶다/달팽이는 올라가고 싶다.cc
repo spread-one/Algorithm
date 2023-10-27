81452e53ee60e13efb944d8aa0983029c666e974
spreadone <111382192+spread-one@users.noreply.github.com>
2023-10-27 11:19:17 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -5,10 +5,6 @@ int main()
 	int a, b, v;
 	cin >> a >> b >> v;
 	int n;
-	//도착하고 남는 경우 (a-b) * n-1 + a > v
-	//딱 도착하는 경우 (a-b) * n-1 + a = v
-	//v/(a-b)
-	//v/(a-b)-a-1
 	for (int i = (v/(a-b)-a-1); i <= (v / (a - b)) + 1; i++)
 	{
 		if (i < 0)
